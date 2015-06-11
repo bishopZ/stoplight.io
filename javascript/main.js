b31e221580e3376ef3be5057a7eb05ecf5233b30
Marc MacLeod <marbemac@gmail.com>
2015-06-11 15:51:25 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -29,7 +29,7 @@ Zepto(function($){
     // 
     $.ajax({
       type: 'POST',
-      url: 'https://stoplightio.herokuapp.com/invites',
+      url: 'https://stoplightapiproduction-20042.onmodulus.net/invites',
       // url: 'http://localhost:3005/invites',
       data: JSON.stringify(data),
       dataType: 'JSON',
