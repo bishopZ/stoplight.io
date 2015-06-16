dfcfc83da8bedc6036554f7eca6629612cde8da6
Marc MacLeod <Marc@Marcs-MacBook-Pro-2.local>
2015-06-15 20:34:20 -0500

    updated splash page, with screenshot

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,22 @@
-Zepto(function($){
+$(document).ready(function() {
 
-  $('.platform-selector li:not(.on)').on('click', function(e) {
+  $(".feature-typer").typed({
+      strings: [
+        "<span style='color: #8e44ad;'>document</span>",
+        "<span style='color: #f39c12;'>debug</span>",
+        "<span style='color: #4EB509;'>validate</span>",
+        "<span style='color: #c0392b;'>test</span>",
+        "<span style='color: #1abc9c;'>mock</span>",
+        "<span style='color: #3880F0;'>collaborate on</span>"
+      ],
+      typeSpeed: 125,
+      loop: true,
+      backDelay: 2500,
+      backSpeed: 75
+  });
+
+  $('.sl-platform-selector li:not(.on)').on('click', function(e) {
     $(this).addClass('on').siblings().removeClass('on');
-    $('.sl-invite-form').addClass('step2');
   });
 
   $('.sl-invite-form').on('submit', function(e) {
@@ -13,24 +27,22 @@ Zepto(function($){
     }
 
     var data = {
-      invite: {
-        platform: $(this).find('li.on').data('name'),
-        email: $.trim($(this).find('input[type="email"]').val())
-      }
+      platform: $(this).find('li.on').data('name'),
+      email: $.trim($(this).find('input[type="email"]').val())
     }
-    if (data.invite.platform.length == 0) {
+    if (data.platform && data.platform.length == 0) {
       alert('Please select a platform.');
       return;
     }
-    if (data.invite.email.length == 0) {
+    if (data.email && data.email.length == 0) {
       alert('Please input a valid email address.');
       return;
     }
-    // 
+    //
     $.ajax({
       type: 'POST',
-      url: 'https://old-api.stoplight.io/invites',
-      // url: 'http://localhost:3005/invites',
+      // url: 'https://api.stoplight.io/invites',
+      url: 'http://localhost:4002/invites',
       data: JSON.stringify(data),
       dataType: 'JSON',
       beforeSend: function() {
@@ -41,7 +53,13 @@ Zepto(function($){
         $('.home').addClass('finished');
       },
       error: function(xhr, errorType, error) {
-        alert('There was an error. Please email hi@stoplight.io and we\'ll sort it out!');
+        $('.sl-invite-form').find('input,button').removeClass('disabled');
+        $('.sl-invite-form button').text('Invite Me');
+        if (xhr.responseJSON) {
+          alert(xhr.responseJSON.error);
+        } else {
+          alert('There was an error. Please email hi@stoplight.io and we\'ll sort it out!');
+        }
       }
     });
   });
