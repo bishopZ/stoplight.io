b7fd96719a35824cc05d204350f29518fb4a9749
Vazha Omanashvili <rainum.ua@gmail.com>
2015-10-14 19:10:44 +0400

    finished with desktop version

--- app/_assets/javascripts/main.js
+++ app/_assets/javascripts/main.js
@@ -6,40 +6,40 @@
     }
   });
 
-  $(".feature-typer").typed({
-    strings: [
-      "<span style='color: #8e44ad;'>document</span>",
-      "<span style='color: #f39c12;'>debug</span>",
-      "<span style='color: #4EB509;'>validate</span>",
-      "<span style='color: #c0392b;'>test</span>",
-      "<span style='color: #1abc9c;'>mock</span>",
-      "<span style='color: #3880F0;'>collaborate on</span>"
-    ],
-    typeSpeed: 125,
-    loop: true,
-    backDelay: 2500,
-    backSpeed: 75
+  $('[href="#invite"]').on('click', function (e) {
+    e.preventDefault()
+
+    var $invite = $('#invite')
+
+    $('html, body').animate({
+      scrollTop: $invite.offset().top - 100
+    }, 500, function () {
+      $invite.find('input[type="email"]').focus()
+    })
   });
 
-  $('.sl-platform-selector li:not(.on)').on('click', function () {
+  $('.form-platform-selector li:not(.on)').on('click', function () {
     $(this).addClass('on').siblings().removeClass('on');
   });
 
-  $('.sl-invite-form').on('submit', function (e) {
+  $('.invite-form').on('submit', function (e) {
     e.preventDefault();
 
-    if ($('.sl-invite-form').find('button').hasClass('disabled')) {
+    if ($('.invite-form').find('button').hasClass('disabled')) {
       return;
     }
 
     var data = {
       platform: $(this).find('li.on').data('name'),
-      email: $.trim($(this).find('input[type="email"]').val())
+      email: $.trim($(this).find('input[type="email"]').val()),
+      page: $(this).data('page')
     }
+
     if (data.platform && data.platform.length == 0) {
       alert('Please select a platform.');
       return;
     }
+
     if (data.email && data.email.length == 0) {
       alert('Please input a valid email address.');
       return;
@@ -51,15 +51,15 @@
       data: JSON.stringify(data),
       dataType: 'JSON',
       beforeSend: function () {
-        $('.sl-invite-form').find('input,button').addClass('disabled');
-        $('.sl-invite-form button').text('Working...');
+        $('.invite-form').find('input,button').addClass('disabled');
+        $('.invite-form button').text('Working...');
       },
       success: function (data, status, xhr) {
         $('.home').addClass('finished');
       },
       error: function (xhr, errorType, error) {
-        $('.sl-invite-form').find('input,button').removeClass('disabled');
-        $('.sl-invite-form button').text('Invite Me');
+        $('.invite-form').find('input,button').removeClass('disabled');
+        $('.invite-form button').text('Invite Me');
         if (xhr.responseJSON) {
           alert(xhr.responseJSON.error);
         } else {
